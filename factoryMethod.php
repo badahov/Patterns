a1d8df8b5b25b178dea7dc347b7e3c5efb4bbc1e
admin <admin@192.168.0.2>
2015-11-09 22:26:24 +0300

    Factory method v1

---
+++ factoryMethod.php
@@ -0,0 +1,40 @@
+<?php
+
+abstract class ApptEncoder {
+    abstract function encode();
+}
+
+class BloggsApptEncoder extends ApptEncoder {
+    function encode() {
+        return "Данные закодированы в формате BloggsCal \n";
+    }
+}
+
+abstract class CommsManager {
+    abstract function getHeaderText();
+    abstract function getApptEncoder();
+    abstract function getFooterText();
+}
+
+class BloggsCommsManager extends CommsManager {
+    function getHeaderText() {
+        return "BloggsCal верхний колонтитул\n";
+    }
+
+    function getApptEncoder() {
+        return new BloggsApptEncoder();
+    }
+
+    function getFooterText() {
+        return "BloggsCal нижний колонтитул\n";
+    }
+}
+
+
+$Bloggs = new BloggsCommsManager();
+
+print $Bloggs->getHeaderText();
+
+print $Bloggs->getApptEncoder()->encode();
+
+print $Bloggs->getFooterText();
